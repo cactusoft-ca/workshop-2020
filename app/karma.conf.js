603602227dc9d02f8ee73841cb2d494de6f86223
Maxime Deslauriers <maxime.deslauriers@cactusoft.ca>
2020-10-22 12:35:20 -0400

    Removed circular dependency

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-var baseConfig = require('./karma.conf')
 module.exports = function (config) {
   baseConfig(config);
   config.browsers = ['ChromeHeadlessNoSandbox'];
