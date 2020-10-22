61cb8a941f0f75eca3aa80fb937785509c4eb374
Antoine Boisier-Michaud <antoine.boisier-michaud@cactusoft.ca>
2020-10-22 12:58:15 -0400

    Renamed method in order to eventually receive other moisture formats

--- server/Workshop.Api/Controllers/MoistureController.cs
+++ server/Workshop.Api/Controllers/MoistureController.cs
@@ -20,7 +20,7 @@ namespace Workshop.Api.Controllers
         [HttpGet]
         public async Task<int> GetMoisture()
         {
-            return await _moistureService.GetMoisture();
+            return await _moistureService.GetRawMoisture();
         }
     }
 }
