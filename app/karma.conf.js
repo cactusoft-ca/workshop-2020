fb7d42a3583a12d65f0bc21144486794d1463113
Maxime Deslauriers <maxime.deslauriers@cactusoft.ca>
2020-10-22 12:38:32 -0400

    Updated karma conf

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -2,7 +2,46 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  baseConfig(config);
-  config.browsers = ['ChromeHeadlessNoSandbox'];
-  config.singleRun = true;
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-teamcity-reporter'),
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    files: [
+      { pattern: 'assets/**/*.*', watched: true, included: false, served: true },
+      { pattern: 'test-assets/**/*.*', watched: true, included: false, served: true }
+    ],
+    proxies: {
+      '/assets/': '/base/assets/',
+      '/test-assets/': '/base/test-assets/',
+    },
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage/gesteplus'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--user-data-dir']
+      }
+    },
+    browserNoActivityTimeout: 60000
+  });
 };
